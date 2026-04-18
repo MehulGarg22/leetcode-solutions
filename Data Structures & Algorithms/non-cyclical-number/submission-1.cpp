2b70236005cda634b9446d1ce0d22db21b16a8e8
Mehul Garg <mehul.garg104@gmail.com>
2026-04-18 21:52:02 +0530

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int>s;
+        while(n!=1){
+            int num=n;
+            int sum=0;
+            while(num!=0){
+                int val=num%10;
+                sum=sum+(val*val);
+                num=num/10;
+            }
+
+            if(s.find(sum)!=s.end()){
+                return false;
+            }
+            if(sum==1) return true;
+            s.insert(sum);
+            n=sum;
+        }
+        return false;
+    }
+};

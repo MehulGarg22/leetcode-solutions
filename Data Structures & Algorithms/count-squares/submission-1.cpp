1cce47075debc2445c644c098f8c1d4efd44bc6a
Mehul Garg <mehul.garg104@gmail.com>
2026-04-19 20:22:40 +0530

    Add: count-squares - submission-1

---
+++ Data Structures & Algorithms/count-squares/submission-1.cpp
@@ -0,0 +1,31 @@
+class CountSquares {
+public:
+
+    vector<pair<int, int>>v;
+    int cnt[1001][1001];
+    CountSquares() {
+        
+    }
+    
+    void add(vector<int> point) {
+        int x=point[0];
+        int y=point[1];
+        cnt[x][y]++;
+        v.push_back({x,y});
+    }
+    
+    int count(vector<int> point) {
+        int x=point[0];
+        int y=point[1];
+        int totalcount=0;
+        for(auto &itr: v){
+            int px=itr.first;
+            int py=itr.second;
+
+            if(abs(px-x)!=0 && abs(px-x)==abs(py-y)){
+                totalcount+=cnt[px][y]*cnt[x][py];
+            }
+        }
+        return totalcount;
+    }
+};

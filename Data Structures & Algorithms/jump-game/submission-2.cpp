397a1f3ea6d653f090b80a0a5e67e882ae73d4f4
Mehul Garg <mehul.garg104@gmail.com>
2026-04-22 23:27:10 +0530

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach=nums[0];
+        for(int i=1; i<nums.size(); i++){
+            if(i>maxReach) return false;
+            maxReach=max(maxReach, i+nums[i]);
+        }
+        return true;
+    }
+};

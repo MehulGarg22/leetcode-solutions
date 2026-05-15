c80b5abdf19f857607c53064d63980e385204aff
Mehul Garg <mehul.garg104@gmail.com>
2026-05-16 00:20:07 +0530

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string s="";
+        int n=min(word1.size(), word2.size());
+        for(int i=0; i<n; i++){
+            s+=word1[i];
+            s+=word2[i];
+        }
+        if(word1.size()<word2.size()){
+            for(int i=n; i<word2.size(); i++){
+                s+=word2[i];
+            }
+        }
+                
+        if(word1.size()>word2.size()){
+            for(int i=n; i<word1.size(); i++){
+                s+=word1[i];
+            }
+        }
+        return s;
+    }
+};

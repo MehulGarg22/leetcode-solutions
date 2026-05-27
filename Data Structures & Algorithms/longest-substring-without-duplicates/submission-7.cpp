3399ec5c97a837c6f4ddbcec9eec44f19fc71c57
Mehul Garg <mehul.garg104@gmail.com>
2026-05-27 22:40:25 +0530

    Add: longest-substring-without-duplicates - submission-7

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-7.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxCount=INT_MIN;
+        for(int i=0; i<s.size(); i++){
+            set<char>ss;
+            for(int j=i; j<s.size(); j++){
+                if(ss.find(s[j])!=ss.end()){
+                    break;
+                }
+                ss.insert(s[j]);
+            }
+            int setSize=ss.size();
+            maxCount=max(maxCount, setSize);
+        }
+        return maxCount;
+    }
+};

bee4c661b2e7f7536a20783dc61eccb6e1aee091
Mehul Garg <mehul.garg104@gmail.com>
2026-05-29 00:21:03 +0530

    Add: find-k-closest-elements - submission-0

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int l=0, r=arr.size()-1;
+       
+      
+        while(r-l>=k){
+            int a=abs(x-arr[l]);
+            int b=abs(x-arr[r]);
+            if(a<=b){
+                r--;
+            }else l++;
+        }
+        return   vector<int>(arr.begin()+l,arr.begin()+r+1);
+    }
+};

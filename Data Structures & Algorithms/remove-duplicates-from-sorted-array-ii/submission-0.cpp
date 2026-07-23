2e3e8a42f2eb4de911c4ff4c4311bbd00e20de5a
Mehul Garg <mehul.garg104@gmail.com>
2026-07-24 00:28:48 +0530

    Add: remove-duplicates-from-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array-ii/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        unordered_map<int, int>mp;
+        vector<int>ans;
+        for(int i=0; i<nums.size(); i++){
+            if(mp[nums[i]]<2){
+                mp[nums[i]]++;
+            }
+        }
+        for(auto& itr: mp){
+            int val=itr.second;
+            while(val!=0){
+                ans.push_back(itr.first);
+                val--;
+            }
+        }
+        for(int i=0; i<ans.size(); i++){
+            nums[i]=ans[i];
+        }
+        sort(nums.begin(), nums.end());
+        return ans.size();
+    }
+};

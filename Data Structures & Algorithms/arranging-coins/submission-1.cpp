039f469432286386d79aa15b61b2fbc53124aa40
Mehul Garg <mehul.garg104@gmail.com>
2026-07-27 23:52:20 +0530

    Add: arranging-coins - submission-1

---
+++ Data Structures & Algorithms/arranging-coins/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int left=1, right=n;
+        int ans=INT_MIN;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            int coin=(mid*(mid+1))/2;
+            if(coin<=n){
+                ans=max(ans, mid);
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+
+        }
+        return ans;
+    }
+};

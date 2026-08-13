7ad1749007ca9c61966fe19b18e7b24785b6329e
Mehul Garg <mehul.garg104@gmail.com>
2026-08-14 01:28:28 +0530

    Add: can-place-flowers - submission-6

---
+++ Data Structures & Algorithms/can-place-flowers/submission-6.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int val=n;
+        if(flowerbed.size()==1 && flowerbed[0]==0) return true; 
+        if(flowerbed[0]==0 && flowerbed[1]==0){
+            flowerbed[0]=1;
+            val--;
+        }
+        for(int i=1; i<flowerbed.size(); i++){
+            if(flowerbed[i]==0 ){
+                if(flowerbed[i-1]==0 && flowerbed[i+1]==0){
+                    flowerbed[i]=1;
+                    val--;
+                }
+            }
+        }
+        if(val<=0) return true;
+        return false;
+    }
+};

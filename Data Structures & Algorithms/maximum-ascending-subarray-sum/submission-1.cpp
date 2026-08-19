a2bb50caab961bcebcee0abd761e1c22f644ddcb
Mehul Garg <mehul.garg104@gmail.com>
2026-08-20 00:39:56 +0530

    Add: maximum-ascending-subarray-sum - submission-1

---
+++ Data Structures & Algorithms/maximum-ascending-subarray-sum/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int ans=nums[0], result=INT_MIN;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]<=nums[i-1]){
+                ans=0;
+            }
+
+                ans+=nums[i];
+                result=max(ans, result);
+            
+        }
+        return result;
+    }
+};

ab9fb71940d300d2757475f5805516773fdc8100
Mehul Garg <mehul.garg104@gmail.com>
2026-09-08 01:10:10 +0530

    Add: final-array-state-after-k-multiplication-operations-i - submission-1

---
+++ Data Structures & Algorithms/final-array-state-after-k-multiplication-operations-i/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        while(k>0){
+            for(int i=0; i<nums.size(); i++){
+                
+                if(nums[i]<minVal){
+
+                }
+            }
+            k--;
+        }
+    }
+};

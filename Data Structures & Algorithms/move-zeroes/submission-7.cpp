bedaf9c39a1c538a2bead2ac984cd3c11c102054
Mehul Garg <mehul.garg104@gmail.com>
2026-09-17 01:28:47 +0530

    Add: move-zeroes - submission-7

---
+++ Data Structures & Algorithms/move-zeroes/submission-7.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        while(i<nums.size()){
+            if(nums[i]==0) break;
+            else{
+                i++;
+            }
+        }
+        int j=i;
+        i=0;
+        while(j<nums.size()){
+            if(nums[j]!=0 && nums[i]==0){
+                swap(nums[i], nums[j]);
+                i++; j++;
+            }else{
+                j++;
+            }
+        }
+    }
+};

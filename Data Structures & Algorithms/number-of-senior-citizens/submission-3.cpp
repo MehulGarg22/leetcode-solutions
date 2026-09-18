aa7348a061a397c6b84578e0272bcd85b08a890e
Mehul Garg <mehul.garg104@gmail.com>
2026-09-18 23:07:08 +0530

    Add: number-of-senior-citizens - submission-3

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-3.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans=0;
+        for(int i=0; i<details.size(); i++){
+            int a= details[i][11];
+        }
+        return ans;
+    }
+};

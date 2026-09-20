a6dac7443411fd3f89c63b7bdab67b3cfb3893d4
Mehul Garg <mehul.garg104@gmail.com>
2026-09-21 01:15:50 +0530

    Add: is-subsequence - submission-6

---
+++ Data Structures & Algorithms/is-subsequence/submission-6.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j=0; 
+
+        for(int i=0; i<s.size(); i++){
+            char first=s[i];
+            bool found=false;
+            while(j<t.size()){
+                char second=t[j];
+                if(first==second){
+                    j++;
+                    found=true;
+                    break;
+                }
+                    j++;
+            }
+            if (found==false) return false;
+        }
+        return true;
+    }
+};
